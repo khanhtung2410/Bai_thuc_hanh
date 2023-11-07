b8a5fc0f20e5ef0687f418f533b2b7f441051e6e
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-07 15:59:37 +0700

    php slide page 19

--- php/index.php
+++ php/index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
-echo "MY FIRST PHP FILE";
-echo "<h3>Hello world!</h3>";
-echo "<select>";
-echo "<option>1</option>";
-echo "<option>2</option>";
-echo "</select>;"
-?>
+    <?php
+    echo "MY FIRST PHP FILE";
+    ?>
+    <h3>Hello world!</h3>
+    <select>
+        <option>1</option>
+        <option>2</option>
+    </select>
 </body>
 </html>
