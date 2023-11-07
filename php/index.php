783568c29d41d844f0750d612572f28e4092080c
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-07 15:57:14 +0700

    slide 17

--- php/index.php
+++ php/index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 echo "MY FIRST PHP FILE";
 echo "<h3>Hello world!</h3>";
@@ -6,3 +14,5 @@ echo "<option>1</option>";
 echo "<option>2</option>";
 echo "</select>;"
 ?>
+</body>
+</html>
