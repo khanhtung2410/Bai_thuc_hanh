a09108d05722bd94980af170e99944f02d4741ac
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-10 10:15:29 +0700

    php slide 48

--- php/index.php
+++ php/index.php
@@ -6,13 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="7-CalResult.php" method="get">
-        <p>First Number: </p>
-        <input type="number" name="firstNumber"><br>
-        <p>Second Number: </p>
-        <input type="number" name="secondNumber"><br>
-        <p></p>
-        <input type="submit" value="Add">
-    </form>
+    Resulr:
+    <?php $result = $_GET["firdtNumber"] + $_GET["secondNumber"];
+    echo $result;
+    ?>
 </body>
 </html>
