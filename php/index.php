4c73ac219aa6019d9fccc33bc5c6d985472f0597
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-10 10:14:14 +0700

    php slide 46

--- php/index.php
+++ php/index.php
@@ -6,7 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email addreess is" <?php echo $_POST["email"];?>
+    <form action="7-CalResult.php" method="get">
+        <p>First Number: </p>
+        <input type="number" name="firstNumber"><br>
+        <p>Second Number: </p>
+        <input type="number" name="secondNumber"><br>
+        <p></p>
+        <input type="submit" value="Add">
+    </form>
 </body>
 </html>
