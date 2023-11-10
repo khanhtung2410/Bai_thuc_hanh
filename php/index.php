6b7b3cb514cd2b9b99c8d7ee114d812a86caddae
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-10 12:12:34 +0700

    slide 30

--- php/index.php
+++ php/index.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/form.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/form.css">
+  <title>Document</title>
 </head>
+
 <body>
-<form action="get-result.php" style="border:1px solid #ccc" method="post">
-  <div class="container">
-    <h1>Sign Up</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
+  <p>bai 1 - 2 bien dau vao - 1 bien ra</p>
+  <?php
+  $so1 = 15;
+  $so2 = 4;
+  if ($so1 > $so2)
+    echo $so1;
+  else echo $so2;
+  ?>
+  <p>bai 2 - 3 bien đau vao - 1 bien ra</p>
+<?php 
+$a = 1;
+$b = 2;
+$c = 3;
+$so_nghiem_x;
+?>
+<p>bai 3 - 1 bien dau vao (string) - 1 bien ra</p>
+<?php 
+$string_vidu = "hoc thoi hh";
+$so_h = 0;
+?>
+<p>bai 4 - 2 bien dau vao - 1 bien ra</p>
+<?php
+$canh_a = 4;
+$canh_b = 5;
+if($a!=$b) echo "hinh chu nhat";
+else echo "hinh vuong" ;
+?>
 
-    <label for="username"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="username" required>
-    <label for="email"><b>Email</b></label>
-    <input type="email" placeholder="Enter Email" name="email" required>
-    <label for="tel"><b>Phonenumber</b></label>
-    <input type="text" placeholder="Enter phonenumber" name="tel" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
-
-    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-
-    <div class="clearfix">
-      <button type="button" class="cancelbtn">Cancel</button>
-      <button type="submit" class="signupbtn">Sign Up</button>
-    </div>
-  </div>
-</form>
 </body>
+
 </html>
