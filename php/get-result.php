d627969d4cb2c39073f6e7398c7d4db7576d0994
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-10 10:35:15 +0700

    signup-form with get.php

---
+++ php/get-result.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+Username cua ban la: <?php echo $_GET["username"]; ?><br>
+Mat khau cua ban la: <?php echo $_GET["email"]; ?><br>   
+Mat khau cua ban la: <?php echo $_GET["tel"]; ?><br>   
+</body>
+<script>
+    alert("Ban dda dang ky thanh cong");
+</script>
+</html>
