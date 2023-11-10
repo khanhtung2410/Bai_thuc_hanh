90382ae2d3b269ad17460c6b8ab93de6f5dd2ff1
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-10 10:12:33 +0700

    php slide 42

--- php/index.php
+++ php/index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    Welcome <?php echo $_GET["name"]; ?><br>
-    Your email addreess is" <?php echo $_GET["email"];?>
+    Welcome <?php echo $_POST["name"]; ?><br>
+    Your email addreess is" <?php echo $_POST["email"];?>
 </body>
 </html>
