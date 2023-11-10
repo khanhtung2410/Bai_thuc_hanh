ffb8a97524266cfac20cdd556fe88213b1b0d655
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-10 10:40:27 +0700

    signup-form with post.php + comment

--- php/get-result.php
+++ php/get-result.php
@@ -6,11 +6,13 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- lay caca thong tin co ban -->
 Username cua ban la: <?php echo $_POST["username"]; ?><br>
 Mat khau cua ban la: <?php echo $_POST["email"]; ?><br>   
 Mat khau cua ban la: <?php echo $_POST["tel"]; ?><br>   
 </body>
 <script>
+    // thong bao dang ky t hanh cong
     alert("Ban dda dang ky thanh cong");
 </script>
 </html>
