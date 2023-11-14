2a5bdff49a9dffc4d0433c5ae084b3b871adafd1
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-14 16:12:54 +0700

    14/11 slide 22 php-p2

--- php/index.php
+++ php/index.php
@@ -5,19 +5,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/form.css">
-  <title>Document</title>
+  <title>Isitnewyear</title>
 </head>
+<style>
+  h1{
+    color: white;
+    font-size: 200px;
+  }
+</style>
+<body style="background-color: black;">
+<?php
+$date_now = date("d");
+$month_now = date("m");
 
-<body>
-<form action="get-result.php" method="post">
-  <label for="math">Toan</label>
-  <input type="text" name="math" required><br>
-  <label for="phy">Ly</label>
-  <input type="text" name="phy" required><br>
-  <label for="chem">Hoa</label>
-  <input type="text" name="chem" required><br>
-  <input type="submit" value="Submit">
-</form>
+if($date_now == "14" && $month_now == "11"){
+  echo "<h1>DUNG</h1>";
+}
+else echo "<h1>SAI</h1>";
+
+?>
 </body>
 
 </html>
