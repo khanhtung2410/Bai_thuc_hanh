90098b94554910c67a6e9a9b537336ecde2aa590
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-14 15:23:49 +0700

    14/11 slide 2 php-p2

--- php/index.php
+++ php/index.php
@@ -9,12 +9,21 @@
 </head>
 
 <body>
-  <form action="get-result.php" method="get">
-  <label for="quit">Do you want to quit?</label><br>  
-  <input type="radio" name="quit" value="yes">Yes <br>
-  <input type="radio" name="quit" value="no">No <br>
-  <input type="submit" value="Confirm">
-  </form>
+<?php
+$T = true;
+$F = false ;
+echo "T=$T <br>";
+echo "F=$F <br>";
+
+$check1 = 10>5;
+$check2 = 20<10;
+$check3 = $check1 && $check2;
+$check4 = $check1 || $check2;
+echo "check1 = $check1 <br>";
+echo "check2 = $check2 <br>";
+echo "check3 = $check3 <br>";
+echo "check4 = $check4 <br>";
+?>
 </body>
 
 </html>
