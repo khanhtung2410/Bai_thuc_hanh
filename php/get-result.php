b5e855e3d4ff2e2d6c04af0c55015a7bcbc1bb34
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-17 10:50:23 +0700

    php slide 35

--- php/get-result.php
+++ php/get-result.php
@@ -9,10 +9,25 @@
 
 <body>
     <?php
-    $exchangeRate = array("USD"=>22300, "EUR"=>27300, "SGD"=>17000 , "JPY"=>120);
-   $amount = $_GET['amount'];
-   $currency = $_GET["currency"];
-   echo " $amount USD is equal " . number_format(($amount * $exchangeRate[$currency]) . "VND");
+    $diem_toan = $_POST['toan'];
+    $diem_ly = $_POST['ly'];
+    $diem_hoa = $_POST['hoa'];
+    $diem_anh = $_POST['anh'];
+    $diem_van = $_POST['van'];
+    $diem_su = $_POST['su'];
+    $TBM = ($diem_toan + $diem_ly + $diem_hoa + $diem_anh + $diem_su + $diem_van) / 6;
+    echo "<h1>EM LA HOC SINH </h1>";
+    if ($diem_anh < 4 || $diem_hoa < 4 || $diem_ly < 4 || $diem_su < 4 || $diem_toan < 4 || $diem_van < 4) {
+        echo "<h1>YEU</h1>";
+    } else {
+        if ($TBM < 5) {
+            echo "<h1>YEU</h1>";
+        } elseif ($TBM < 6.4) {
+            echo "<h1>TRUNG BINH</h1>";
+        } elseif ($TBM < 7.9) {
+            echo "<h1>KHA</h1>";
+        } else  echo "<h1>GIOI</h1>";
+    }
     ?>
 </body>
 
--- php/index.php
+++ php/index.php
@@ -8,17 +8,60 @@
   <title>Isitnewyear</title>
 </head>
 
-<body >
-<form action="get-result.php" method="post">
-  <label for="sex">Gioi tinh</label><br>
-  <input type="radio" name="sex" value="male">Nam
-  <input type="radio" name="sex" value="female">Nu <br>
-  <label for="height">Chieu cao</label><br>
-  <input type="text" name="height" required><br>
-  <label for="weight">Can nang</label><br>
-  <input type="text" name="weight" required><br>
-  <input type="submit" value="Submit">
-</form>
+<body>
+  <form action="get-result.php" method="post">
+    <label for="toan">Toan</label>
+    <input type="text" name="toan" id="toan" required><br>
+    <label for="ly">Ly</label>
+    <input type="text" name="ly" id="ly" required><br>
+    <label for="hoa">Hoa</label>
+    <input type="text" name="hoa" id="hoa" required><br>
+    <label for="anh">Anh</label>
+    <input type="text" name="anh" id="anh" required><br>
+    <label for="van">Van</label>
+    <input type="text" name="van" id="van" required><br>
+    <label for="su">Su</label>
+    <input type="text" name="su" id="su" required><br>
+    <input type="submit" value="Submit">
+  </form>
 </body>
+<script>
+  document.getElementById("toan").onkeyup = function() {
+    if (document.getElementById("toan").value > 10|| document.getElementById("toan").value < 0) {
+      alert("Sai dinh dang");
+      document.getElementById("toan").value = ' ';
+    }
+  }
+  document.getElementById("ly").onkeyup = function() {
+    if (document.getElementById("ly").value > 10|| document.getElementById("ly").value < 0) {
+      alert("Sai dinh dang");
+      document.getElementById("ly").value = ' ';
+    }
+  }
+  document.getElementById("hoa").onkeyup = function() {
+    if (document.getElementById("hoa").value > 10|| document.getElementById("hoa").value < 0) {
+      alert("Sai dinh dang");
+      document.getElementById("hoa").value = '';
+    }
+  }
+  document.getElementById("anh").onkeyup = function() {
+    if (document.getElementById("anh").value > 10|| document.getElementById("anh").value < 0) {
+      alert("Sai dinh dang");
+      document.getElementById("anh").value = '';
+    }
+  }
+  document.getElementById("van").onkeyup = function() {
+    if (document.getElementById("van").value > 10 || document.getElementById("van").value < 0) {
+      document.getElementById("van").value = '';
+      alert("Sai dinh dang");
+    }
+  }
+  document.getElementById("su").onkeyup = function() {
+    if (document.getElementById("su").value > 10 || document.getElementById("su").value < 0) {
+      alert("Sai dinh dang");
+      document.getElementById("su").value = ' ';
+    }
+  }
+</script>
 
 </html>
