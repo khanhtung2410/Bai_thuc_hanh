e74dda9efbeb9bec47b906def7688f66158e8646
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-17 10:04:34 +0700

    php slide 28

--- php/get-result.php
+++ php/get-result.php
@@ -9,18 +9,18 @@
 
 <body>
     <?php
-    $currency = $_GET['currency'];
-    echo " $amount $currency is equal ";
-    switch ($currency) {
-        case "UDS":
-            echo $amount * $USD;
-        case "EUR":
-            echo $amount * $EUR;
-        case "SGD":
-            echo $amount * $SGD;
-        case "JPY":
-            echo $amount * $JPY;
-    }
+    $exchangeRate = array(22300, 27300, 17000 , 120);
+    $exchangeRate[] = 600;
+    $exchangeRate[] = 16000;
+    $exchangeRate[0] = 22200;
+    echo "<h3>Exchange rate to VND</h3>";
+    echo "USD: $exchangeRate[0]<br>"; 
+    echo "EUR: $exchangeRate[1]<br>"; 
+    echo "SGD: $exchangeRate[2]<br>"; 
+    echo "JPY: $exchangeRate[3]<br>"; 
+    echo "CHN: $exchangeRate[4]<br>"; 
+    echo "AUD: $exchangeRate[5]<br>"; 
+
     ?>
 </body>
 
