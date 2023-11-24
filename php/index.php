14c3d43fea4f737df4a29fe281713d07f1e7f56e
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-24 09:56:07 +0700

    24/11 - php -p3 -slide12

--- php/index.php
+++ php/index.php
@@ -9,59 +9,12 @@
 </head>
 
 <body>
-  <form action="get-result.php" method="post">
-    <label for="toan">Toan</label>
-    <input type="text" name="toan" id="toan" required><br>
-    <label for="ly">Ly</label>
-    <input type="text" name="ly" id="ly" required><br>
-    <label for="hoa">Hoa</label>
-    <input type="text" name="hoa" id="hoa" required><br>
-    <label for="anh">Anh</label>
-    <input type="text" name="anh" id="anh" required><br>
-    <label for="van">Van</label>
-    <input type="text" name="van" id="van" required><br>
-    <label for="su">Su</label>
-    <input type="text" name="su" id="su" required><br>
-    <input type="submit" value="Submit">
-  </form>
+<?php
+$x=1;
+while($x<=10){
+  echo "Day la so: $x <br>";
+} 
+?>
 </body>
-<script>
-  document.getElementById("toan").onkeyup = function() {
-    if (document.getElementById("toan").value > 10|| document.getElementById("toan").value < 0) {
-      alert("Sai dinh dang");
-      document.getElementById("toan").value = ' ';
-    }
-  }
-  document.getElementById("ly").onkeyup = function() {
-    if (document.getElementById("ly").value > 10|| document.getElementById("ly").value < 0) {
-      alert("Sai dinh dang");
-      document.getElementById("ly").value = ' ';
-    }
-  }
-  document.getElementById("hoa").onkeyup = function() {
-    if (document.getElementById("hoa").value > 10|| document.getElementById("hoa").value < 0) {
-      alert("Sai dinh dang");
-      document.getElementById("hoa").value = '';
-    }
-  }
-  document.getElementById("anh").onkeyup = function() {
-    if (document.getElementById("anh").value > 10|| document.getElementById("anh").value < 0) {
-      alert("Sai dinh dang");
-      document.getElementById("anh").value = '';
-    }
-  }
-  document.getElementById("van").onkeyup = function() {
-    if (document.getElementById("van").value > 10 || document.getElementById("van").value < 0) {
-      document.getElementById("van").value = '';
-      alert("Sai dinh dang");
-    }
-  }
-  document.getElementById("su").onkeyup = function() {
-    if (document.getElementById("su").value > 10 || document.getElementById("su").value < 0) {
-      alert("Sai dinh dang");
-      document.getElementById("su").value = ' ';
-    }
-  }
-</script>
 
 </html>
