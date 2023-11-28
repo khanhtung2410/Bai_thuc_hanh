2104373772fb5043cb27e2a9eaad3ae88d99de56
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-28 16:02:53 +0700

    28/11 - php3 - slide20

--- php/index.php
+++ php/index.php
@@ -9,36 +9,20 @@
 </head>
 
 <body>
-  <h1>checkhtml</h1>
-<form action="get-result.php" method="post">
-  <label for="ngay">ngay: </label>
-  <select name="ngay" id="chon-ngay">
-    <?php 
-    for($date=1;$date<=31;$date+=+1){
-      echo "<option value=$date>$date</option>";
-    }
-    ?>
-  </select>
-  <label for="thang">thang: </label>
-  <select name="thang" id="chon-thang">
-    <?php 
-    for($month=1;$month<=12;$month+=+1){
-      echo "<option value=$month>$month</option>";
-    }
-    ?>
-  </select>
-  <label for="nam">nam: </label>
-  <select name="nam" id="chon-nam">
-    <?php
-    $today = getdate();
-    $namhientai = $today["year"];
-    for($nam=1990;$nam<=$namhientai;$nam+=+1){
-      echo "<option value=$nam>$nam</option>";
-    }
-    ?>
-  </select>
-  <input type="submit">
-</form>
+<?php
+$items = array('ahri.jpg','ryze.jpg','hecarim.jpg');
+foreach($items as $item){
+  echo "<img src='/php/$item' width=50px height=50px alt='$item'>";
+
+}
+?>
+<h1>function</h1>
+<?php 
+$item = array('ahri.jpg','hecarim.jpg');
+foreach($items as $item){
+  echo "<img scr='images/$item' width=50px height=50px alt='$item'>";
+}
+?>
 </body>
 
 </html>
