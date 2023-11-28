c4b720cb9f76a79dd98f10a2dfcd33f4e14640ee
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-11-28 16:22:18 +0700

    28/11 - php3 - slide27

--- php/index.php
+++ php/index.php
@@ -9,32 +9,27 @@
 </head>
 
 <body>
-    <?php
-    $diem_toan = $_POST['toan'];
-    $diem_ly = $_POST['ly'];
-    $diem_hoa = $_POST['hoa'];
-    $diem_anh = $_POST['anh'];
-    $diem_van = $_POST['van'];
-    $diem_su = $_POST['su'];
-    function TBM($a,$b,$c,$d,$e,$f){
-      $diem = ($a + $b +$c+$d+$e+$f)/6;
-      return $diem;
-    }
-    echo "<h1>EM LA HOC SINH </h1>";
-    if ($diem_anh < 4 || $diem_hoa < 4 || $diem_ly < 4 || $diem_su < 4 || $diem_toan < 4 || $diem_van < 4) {
-        echo "<h1>YEU</h1>";
-    } else {
-      $TMB = TBM($diem_anh,$diem_hoa,$diem_ly,$diem_su,$diem_toan,$diem_van);
-        if ($TBM < 5) {
-            echo "<h1>YEU</h1>";
-        } elseif ($TBM < 6.4) {
-            echo "<h1>TRUNG BINH</h1>";
-        } elseif ($TBM < 7.9) {
-            echo "<h1>KHA</h1>";
-        } else  echo "<h1>GIOI</h1>";
-    }
-    ?>
+ <h3>currency exchange</h3>
+ <form action="" method="post">
+  <p>amount</p>
+  <input type="number" name="amount">
+  <p>select currency</p>
+  <select name="currency">
+    <option value="usd">usd</option>
+    <option value="eur">eur</option>
+  <option value="sgd">singapore dolar</option>
+  <option value="jpy">japanese yen</option>
+  </select>
+  <input type="submit" value="convert">
+ </form>
 </body>
-
+<?php
+$exchangerate = array("usd"=>22300,'eur'=>27300,'sgd'=>17000,'jpy'=>200)
+if (isset($_POST['amount'])) {
+  $amount = $_POST['amount'];
+  $currency = $_POST['currency'];
+  echo "$amount usd is equal". number_format($amount * $exchangerate['currency']). "vnd";
+}
+?>
 
 </html>
